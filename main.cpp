1c13f9c1a0ba3fe09bdd132ce49fbd1a8dbfdd66
Janpopadiuk <popadiukjan@gmail.com>
2025-11-25 20:15:36 +0100

    Algirithm itself

---
+++ main.cpp
@@ -0,0 +1,105 @@
+#include <vector>
+#include <bits/stdc++.h>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+class TrackList
+{
+public:
+    vector<int> tracklist;
+    vector<int> qList;
+    queue<int> Queue;
+    queue<int> LoopQueue;
+    TrackList(){
+
+        tracklist.push_back(3);
+        tracklist.push_back(3);
+        tracklist.push_back(2);
+        tracklist.push_back(1);
+        tracklist.push_back(1);
+    }
+    void TLtoQL()
+    {
+        for(int i = 0; i < 5;i++)
+        {
+            for(int j = 0; j < tracklist[i]; j++)
+            {
+                qList.push_back(i);
+            }
+
+        }
+        Shuffle();
+    }
+
+    void Shuffle()
+    {
+        srand(time(nullptr));
+        int tmp = 0;
+        int ran = 0;
+        while(!qList.empty())
+        {
+            ran = rand()%qList.size();
+            tmp = qList[ran];
+            Queue.push(tmp);
+            for (int i = (int)qList.size() - 1; i >= 0; i--)
+            {
+                if (qList[i] == tmp)
+                {
+                    qList.erase(qList.begin() + i);
+                }
+            }
+        }
+
+    }
+
+
+};
+
+void Display(TrackList* tmp)
+{
+        cout<<"Now playing: " << tmp->Queue.front()<<endl;
+}
+
+void Next(TrackList* tmp)
+{
+
+}
+
+void Start(TrackList* tmp, bool Shuffle)
+{
+        if (Shuffle)
+        {
+            tmp->TLtoQL();
+        tmp->LoopQueue = tmp->Queue;
+        }
+        else
+        {
+            for (int i = (int)tmp->tracklist.size(); i >= 0; i--)
+            {
+                tmp->Queue.push(tmp->tracklist[i]);
+
+            }
+            Display(tmp);
+        }
+
+}
+int main(){
+        TrackList* tmp = new TrackList();
+        bool Shuffle = false;
+        cout<<"Shuffle? 0/1"<<endl;
+        cin>>Shuffle;
+        Start(tmp, Shuffle);
+
+        // tmp->TLtoQL();
+        // tmp->LoopQueue = tmp->Queue;
+        // while (!tmp->Queue.empty()) {
+        //     cout<< tmp->Queue.front()<< " ";
+        //
+        //     tmp->Queue.pop();
+        //
+        // }
+delete tmp;
+
+    return 0;
+}
