9b334e2761dcd79d0fb229859c21dd9e1e75f24d
JanPopadiuk <popadiukjan@gmail.com>
2025-11-26 12:15:21 +0100

    Changed queue to a vector for

--- main.cpp
+++ main.cpp
@@ -9,8 +9,7 @@ class TrackList
 public:
     vector<int> tracklist;
     vector<int> qList;
-    queue<int> Queue;
-    queue<int> LoopQueue;
+    vector<int> Queue;
     TrackList(){
 
         tracklist.push_back(3);
@@ -41,7 +40,7 @@ public:
         {
             ran = rand()%qList.size();
             tmp = qList[ran];
-            Queue.push(tmp);
+            Queue.push_back(tmp);
             for (int i = static_cast<int>(qList.size()) - 1; i >= 0; i--)
             {
                 if (qList[i] == tmp)
@@ -56,54 +55,66 @@ public:
 
 };
 
-void Display(const TrackList* tmp)
+void Display(const TrackList* tmp, int curr)
 {
-        cout<<"Now playing: " << tmp->Queue.front()<<endl;
+        cout<<"Now playing: " << tmp->Queue[curr]<<endl;
 }
 
-void Next(TrackList* tmp)
-{
-    tmp->Queue.pop();
-    Display(tmp);
-}
 
-void Start(TrackList* tmp, bool Shuffle)
+
+void Start(TrackList* tmp, bool Shuffle, int curr)
 {
         if (Shuffle)
         {
             tmp->TLtoQL();
-        tmp->LoopQueue = tmp->Queue;
-            Display(tmp);
+        
+            Display(tmp, curr);
         }
         else
         {
             for (int i = static_cast<int>(tmp->tracklist.size()); i >= 0; i--)
             {
-                tmp->Queue.push(tmp->tracklist[i]);
+                tmp->Queue.push_back(tmp->tracklist[i]);
 
             }
-            Display(tmp);
+            Display(tmp,curr);
         }
 
 }
 int main(){
+		int Current = 0;
         auto* tmp = new TrackList();
         bool Shuffle = false;
         int choice = 0;
         cout<<"Shuffle? 0/1"<<endl;
         cin>>Shuffle;
-        Start(tmp, Shuffle);
+        Start(tmp, Shuffle, Current);
 
-        while (!tmp->Queue.empty())
+        while(Current < static_cast<int>(tmp->Queue.size()))
         {
             cout<<"Next/Previous/Close 1/2/0"<<endl;
             cin>>choice;
             switch(choice) {
                 case 1:
-                    Next(tmp);
+					Current++;
+					if(!Current < static_cast<int>(tmp->Queue.size())
+					{
+                    Display(tmp,Current);
+					}else
+					{
+						Current = 0;
+						Display(tmp,Current);
+					}
                     break;
                 case 2:
-                    //Previous();
+                    Current--;
+                    if(!Current > 0)
+                    {
+                    Display(tmp,Current);
+                    }else{
+                    Current = static_cast<int>(tmp->Queue.size() - 1;
+                    Display(tmp,Current);
+					}
                     break;
                 default:
                     delete tmp;
