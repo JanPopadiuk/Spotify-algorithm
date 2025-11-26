dcb2bad7c9bd3863d022c7344c7144054daec37a
Janpopadiuk <popadiukjan@gmail.com>
2025-11-26 21:02:41 +0100

    Shuffling added again

--- main.cpp
+++ main.cpp
@@ -9,121 +9,117 @@ public:
     vector<int> tracklist;
     vector<int> qList;
     vector<int> Queue;
-    TrackList(){
 
+    TrackList()
+    {
         tracklist.push_back(3);
         tracklist.push_back(3);
         tracklist.push_back(2);
         tracklist.push_back(1);
         tracklist.push_back(1);
     }
+
     void TLtoQL()
     {
-        for(int i = 0; i < 5;i++)
+        qList.clear();
+        Queue.clear();
+
+        for(int i = 0; i < (int)tracklist.size(); i++)
         {
             for(int j = 0; j < tracklist[i]; j++)
             {
                 qList.push_back(i);
             }
-
         }
+
         Shuffle();
     }
 
     void Shuffle()
     {
         srand(time(nullptr));
-        int tmp = 0;
-        int ran = 0;
         while(!qList.empty())
         {
+            int ran = rand() % qList.size();
+            int tmp = qList[ran];
 
-            ran = rand()%static_cast<int>(qList.size());
-            tmp = qList[ran];
             Queue.push_back(tmp);
-            for (int i = static_cast<int>(qList.size()) - 1; i >= 0; i--)
+
+
+            for (int i = qList.size() - 1; i >= 0; i--)
             {
                 if (qList[i] == tmp)
-                {
                     qList.erase(qList.begin() + i);
-                }
             }
         }
-
     }
-
-
 };
 
-void Display(int curr)
+void Display(int currTrackID)
 {
-        cout<<"Now playing: " << curr<<endl;
+    cout << "Now playing: " << currTrackID << endl;
 }
 
-
-
-void Start(TrackList* tmp, bool Shuffle, int curr)
+void Start(TrackList* tmp, bool Shuffle, int &curr)
 {
-        if (Shuffle)
-        {
-            tmp->TLtoQL();
-        
-            Display(curr);
-        }
-        else
-        {
-            for (int i = static_cast<int>(tmp->tracklist.size()); i >= 0; i--)
-            {
-                tmp->Queue.push_back(tmp->tracklist[i]);
+    if (Shuffle)
+    {
+        tmp->TLtoQL();
 
-            }
-            Display(curr);
-        }
+        curr = 0;
+        Display(tmp->Queue[curr]);
+    }
+    else
+    {
+        tmp->Queue.clear();
 
+
+        for (int i = 0; i < (int)tmp->tracklist.size(); i++)
+            tmp->Queue.push_back(i);
+
+        curr = 0;
+        Display(tmp->Queue[curr]);
+    }
 }
-int main(){
-		int Current = 0;
-        auto* tmp = new TrackList();
-        bool Shuffle = false;
-        int choice = 0;
-        cout<<"Shuffle? 0/1"<<endl;
-        cin>>Shuffle;
-        Start(tmp, Shuffle, Current);
 
-        while(Current < static_cast<int>(tmp->Queue.size()))
+int main()
+{
+    int Current = 0;
+    auto* tmp = new TrackList();
+    bool Shuffle = false;
+    int choice = 0;
+
+    cout << "Shuffle? 0/1: ";
+    cin >> Shuffle;
+
+    Start(tmp, Shuffle, Current);
+
+    while (true)
+    {
+        cout << "Next/Previous/Close 1/2/0: ";
+        cin >> choice;
+
+        if (choice == 0)
         {
-            cout<<"Next/Previous/Close 1/2/0"<<endl;
-            cin>>choice;
-            switch(choice) {
-                case 1:
-					Current++;
-					if(Current < static_cast<int>(tmp->Queue.size())-1)
-					{
-                    Display(Current);
-					}else
-					{
-						Current = 0;
-						Display(Current);
-					}
-                    break;
-                case 2:
-                    Current--;
-                    if(Current < 0)
-                    {
-                        Current = static_cast<int>(tmp->Queue.size())-2;
-                        Display(Current);
-                    }else{
-                        Display(Current);
-					}
-                    break;
-                default:
-                    delete tmp;
-                    return 0;
-
-            }
+            delete tmp;
+            return 0;
         }
 
+        if (choice == 1)  // NEXT
+        {
+            Current++;
+            if (Current >= (int)tmp->Queue.size())
+                Current = 0;
 
+            Display(tmp->Queue[Current]);
+        }
+        else if (choice == 2)  // PREVIOUS
+        {
+            Current--;
+            if (Current < 0)
+                Current = tmp->Queue.size() - 1;
 
-
+            Display(tmp->Queue[Current]);
+        }
+    }
 }
